fb2d9a5503da26c2a56010b5b71e2358d915a1d0
Felipe Simões <felsimoes@gmail.com>
2019-11-03 19:10:24 -0300

    Projeto, Classe Program e Classe Product - Criado

---
+++ src/application/Program.java
@@ -0,0 +1,24 @@
+
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * O programa deverá coletar o preço de vários produtos retirados de um arquivo .csv
+ * Retornar o produto com o valor mais alto da listagem e a porcentagem dele em relação ao total 
+ */
+
+public class Program {
+
+    public static void main(String[] args) {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        
+        
+        sc.close();
+    }
+}
